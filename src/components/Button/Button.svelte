3ae371c9d3f58906b1ffcb1b1d7e5e300c124f88
Seth Knights <knightss@catlin.edu>
2021-03-20 16:24:44 -0700

    Basic button start, still lots more to do.

--- src/components/Button/Button.svelte
+++ src/components/Button/Button.svelte
@@ -1,9 +1,60 @@
 <script lang="ts">
-    // your script goes here
+    
+    let userClasses: string = '';
+    export { userClasses as class };
+    export let size: string = 'default';
+    export let style: string = null;
+    export let type: string = null;
+    export let rounded: boolean = false;
+    export let disabled: boolean = null;
+    export let filled: boolean = false;
+    export let icon: string = null;
+    export let href: string = null;
+    export let ref: HTMLElement = null;
 </script>
 
-<style lang="scss" srce="./Button.scss" global>
-    /* your styles go here */
+<style lang="scss" src="./Button.scss" global>
 </style>
 
-<!-- markup (zero or more items) goes here -->
+{#if href}
+    <a
+        bind:this={ref}
+        {href}
+        class="pf-btn size-{size} {userClasses}"
+        class:icon
+        class:disabled
+        class:rounded
+        class:filled
+        {style}
+        {...$$restProps}
+    >
+        {#if icon}
+            <span class="pf-btn__icon material-icons">{icon}</span>
+        {/if}
+        <span class="pf-btn__content">
+            <slot></slot>
+        </span>
+    </a>
+{:else}
+    <button
+        bind:this={ref}
+        class="pf-btn size-{size} {userClasses}"
+        class:icon
+        class:disabled
+        class:rounded
+        class:filled
+        {type}
+        {style}
+        {disabled}
+        on:click
+        on:dblclick
+        {...$$restProps}
+    >
+        {#if icon}
+            <span class="pf-btn__icon material-icons">{icon}</span>
+        {/if}
+        <span class="pf-btn__content">
+            <slot></slot>
+        </span>
+    </button>
+{/if}
--- src/demo/pages/Index.svelte
+++ src/demo/pages/Index.svelte
@@ -5,5 +5,18 @@
 <main>
   <h1>Packetframe Design Demo</h1>
   <a href="https://github.com/packetframe/design">https://github.com/packetframe/design</a>
-  <Button />
+  <br>
+  <Button>Export</Button>
+  <Button rounded>Export</Button>
+  <Button icon="add">Export</Button>
+  <Button rounded icon="add">Export</Button>
 </main>
+
+<style>
+  main {
+    background-color: #121212;
+  }
+  h1 {
+    color: white;
+  }
+</style>
