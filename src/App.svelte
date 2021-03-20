a5ef63654422d005b5fdaa1013a9e613d40a0d3a
Seth Knights <knightss@catlin.edu>
2021-03-19 22:48:01 -0700

    More setup.

--- src/App.svelte
+++ src/App.svelte
@@ -11,23 +11,7 @@
 	$color: red;
 
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-		background: $color;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		width: 100%;
+		min-height: 100vh;
 	}
 </style>
