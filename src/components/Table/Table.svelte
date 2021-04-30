18b963e2bfbd68a0158d246646c14440a226600c
Nate Sales <nate@natesales.net>
2021-04-29 23:25:47 -0700

    start table component

---
+++ src/components/Table/Table.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let title: string = "";
+</script>
+
+<style lang="scss" src="./Table.scss" global />
+
+<main>
+  <div>
+    <h2>{title}</h2>
+    <span>
+      <slot name="header" />
+    </span>
+  </div>
+
+  <table>
+    <slot />
+  </table>
+</main>
