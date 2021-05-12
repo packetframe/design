a446d48d523de94833a7bdf1cb336a0a60cc7fa1
Seth Knights <knightss@catlin.edu>
2021-05-12 13:38:47 -0700

    Chevron and stuff.

--- src/components/Select/Select.svelte
+++ src/components/Select/Select.svelte
@@ -1,11 +1,13 @@
 <script>
   import Select from "svelte-select";
   export let items;
+  export let showIndicator = true;
+  export let isClearable = false;
 </script>
 
 <style lang="scss" src="./Select.scss" global>
 </style>
 
 <div class="pf-select">
-  <Select {items} on:select {$$restProps}></Select>
+  <Select {items} {isClearable} {showIndicator} on:select {$$restProps}></Select>
 </div>
