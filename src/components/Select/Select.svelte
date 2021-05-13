a1cbbf6921aeef3d6b363dee0346d27f47f239b3
Seth Knights <knightss@catlin.edu>
2021-05-12 18:52:58 -0700

    Possibility to use icon -- ish.

--- src/components/Select/Select.svelte
+++ src/components/Select/Select.svelte
@@ -3,11 +3,14 @@
   export let items;
   export let showIndicator = true;
   export let isClearable = false;
+  export let icon = null;
+
+  import AddIcon from './AddIcon.svelte';
 </script>
 
 <style lang="scss" src="./Select.scss" global>
 </style>
 
-<div class="pf-select">
-  <Select {items} {isClearable} {showIndicator} on:select {$$restProps}></Select>
+<div class="pf-select" class:icon>
+  <Select Icon={icon ? AddIcon : null} {items} {isClearable} {showIndicator} on:select {$$restProps}></Select>
 </div>
