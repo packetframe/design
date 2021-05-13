8880b2789589efd668a958ff44072b21263d8301
Nate Sales <nate@natesales.net>
2021-05-12 22:27:35 -0700

    add record boilerplate

---
+++ src/components/Record/Record.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let label: string = "";
+  export let rType: string = "";
+  export let ttl: number = 0;
+  export let value: number = 0;
+</script>
+
+<style lang="scss" src="./Record.scss" global />
+
+<main>
+</main>
