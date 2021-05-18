bc9cb3dae1d6344dd750c2d3aaed99bb47b6b66b
Nate Sales <nate@natesales.net>
2021-05-17 23:07:37 -0700

    toggle record dropdown on click

--- src/components/Record/Record.svelte
+++ src/components/Record/Record.svelte
@@ -11,18 +11,14 @@
   export let zebra: boolean = false;
 
   let open: boolean = false;
-
-  const handleDropdown = () => {
-    open = !open;
-  };
 </script>
 
-<tr class="pf-record" class:zebra class:open>
+<tr class="pf-record" class:zebra class:open on:click={() => open = !open}>
   <td class="pf-record__label">{record.label}</td>
   <td id="col2" class="pf-record__type">{record.type}</td>
   <td class="pf-record__ttl">{record.ttl}</td>
   <td class="pf-record__value">{record.value}</td>
-  <td on:click={handleDropdown} class="pf-record__arrow">
+  <td class="pf-record__arrow">
     <span class="material-icons-round">expand_more</span>
   </td>
 </tr>
