4cdcb9916fa6af97adc27595de19bb6096b9d467
Seth Knights <knightss@catlin.edu>
2021-05-20 22:47:48 -0700

    Fixed alignment issues, but now the button is too big.

--- src/components/RecordField/RecordField.svelte
+++ src/components/RecordField/RecordField.svelte
@@ -33,25 +33,28 @@
   }
 </script>
 
-<style lang="scss" src="./RecordField.scss" global></style>
+<style lang="scss" src="./RecordField.scss" global>
+</style>
 
 <div class="pf-record-field">
-  <Input bind:value={record.label} label="Label" />
-  <span class="pf-record-field__small-select"><Select label="Type" items={recordTypes} on:select={handleRecordSelect} bind:selectedValue={recordTypes[0]} isDisabled={isInDropdown} /></span>
-  <Input class="small" type="number" label="TTL" placeholder="86400" min="0" bind:value={record.ttl} />
+  <div class="pf-record-field__row">
+    <Input bind:value={record.label} label="Label" />
+    <span class="pf-record-field__small-select"><Select label="Type" items={recordTypes} on:select={handleRecordSelect} bind:selectedValue={recordTypes[0]} isDisabled={isInDropdown} /></span>
+    <Input class="small" type="number" label="TTL" placeholder="86400" min="0" bind:value={record.ttl} />
 
-  {#if type === "A"}
-    <Input bind:value={record.value} label="IPv4 Address" />
-  {:else if type === "AAAA"}
-    <Input bind:value={record.value} label="IPv6 Address" />
-  {:else if type === "MX"}
-    <Input type="number" label="Priority" min="0" />
-    <Input bind:value={record.value} label="Server" />
-  {:else if type === "NS"}
-    <Input bind:value={record.value} label="Nameserver" />
-  {:else if type === "TXT"}
-    <Input bind:value={record.value} label="Value" />
-  {/if}
-  <Button variant="secondary" icon={record.proxied ? "cloud_queue" : "cloud_off"} on:click={() => record.proxied = !record.proxied} class="ma-2" />
-  <Button variant="secondary" class="ma-2">Save</Button>
+    {#if type === "A"}
+      <Input bind:value={record.value} label="IPv4 Address" />
+    {:else if type === "AAAA"}
+      <Input bind:value={record.value} label="IPv6 Address" />
+    {:else if type === "MX"}
+      <Input type="number" label="Priority" min="0" />
+      <Input bind:value={record.value} label="Server" />
+    {:else if type === "NS"}
+      <Input bind:value={record.value} label="Nameserver" />
+    {:else if type === "TXT"}
+      <Input bind:value={record.value} label="Value" />
+    {/if}
+    <Button variant="secondary" icon={record.proxied ? "cloud_queue" : "cloud_off"} on:click={() => record.proxied = !record.proxied}/>
+  </div>
+  <Button variant="secondary">Save</Button>
 </div>
