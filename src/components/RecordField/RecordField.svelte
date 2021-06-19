c8ca5103198d3bf4abfd9e32c1026e016a6a8fcd
Nate Sales <nate@natesales.net>
2021-06-19 14:15:11 -0700

    add SRV record

--- src/components/RecordField/RecordField.svelte
+++ src/components/RecordField/RecordField.svelte
@@ -1,60 +1,77 @@
 <script lang="ts">
-  import Input from "../Input";
-  import Button from "../Button";
-  import Select from "../Select/Select.svelte";
-  import { onMount } from "svelte";
+    import Input from "../Input";
+    import Button from "../Button";
+    import Select from "../Select/Select.svelte";
+    import {onMount} from "svelte";
 
-  export let record = {
-    label: "",
-    ttl: 86400,
-    value: "",
-    proxied: false
-  };
-  export let type = "A";
-  export let isInDropdown = false;
+    export let record = {
+        label: "",
+        ttl: 86400,
+        value: "",
+        proxied: false
+    };
+    export let type = "A";
+    export let isInDropdown = false;
 
-  let recordTypes = [
-    { value: "A", label: "A" },
-    { value: "AAAA", label: "AAAA" },
-    { value: "MX", label: "MX" },
-    { value: "NS", label: "NS" },
-    { value: "TXT", label: "TXT" }
-  ];
+    let recordTypes = [
+        {value: "A", label: "A"},
+        {value: "AAAA", label: "AAAA"},
+        {value: "MX", label: "MX"},
+        {value: "TXT", label: "TXT"},
+        {value: "NS", label: "NS"},
+        {value: "SRV", label: "SRV"}
+    ];
 
-  // If the component is in the record edit dropdown, set a static record type
-  onMount(() => {
-    if (isInDropdown) {
-      recordTypes = [{ value: type, label: type }];
+    // If the component is in the record edit dropdown, set a static record type
+    onMount(() => {
+        if (isInDropdown) {
+            recordTypes = [{value: type, label: type}];
+        }
+    });
+
+    function handleRecordSelect(event) {
+        type = event.detail.value;
     }
-  });
-
-  function handleRecordSelect(event) {
-    type = event.detail.value;
-  }
 </script>
 
-<style lang="scss" src="./RecordField.scss" global>
+<style global lang="scss" src="./RecordField.scss">
 </style>
 
 <div class="pf-record-field">
-  <div class="pf-record-field__row">
-    <Input bind:value={record.label} label="Label" />
-    <span class="pf-record-field__small-select"><Select label="Type" items={recordTypes} on:select={handleRecordSelect} bind:selectedValue={recordTypes[0]} isDisabled={isInDropdown} /></span>
-    <Input class="small" type="number" label="TTL" placeholder="86400" min="0" bind:value={record.ttl} />
+    <div class="pf-record-field__row">
+        <Input bind:value={record.label} label="Label"/>
+        <span class="pf-record-field__small-select">
+            <Select bind:selectedValue={recordTypes[0]} isDisabled={isInDropdown}
+                    items={recordTypes}
+                    label="Type"
+                    on:select={handleRecordSelect}/>
+        </span>
+        <Input bind:value={record.ttl} class="small" label="TTL" min="0" placeholder="86400" type="number"/>
 
-    {#if type === "A"}
-      <Input bind:value={record.value} label="IPv4 Address" />
-    {:else if type === "AAAA"}
-      <Input bind:value={record.value} label="IPv6 Address" />
-    {:else if type === "MX"}
-      <Input type="number" label="Priority" min="0" />
-      <Input bind:value={record.value} label="Server" />
-    {:else if type === "NS"}
-      <Input bind:value={record.value} label="Nameserver" />
-    {:else if type === "TXT"}
-      <Input bind:value={record.value} label="Value" />
-    {/if}
-    <Button variant="secondary" icon={record.proxied ? "cloud_queue" : "cloud_off"} on:click={() => record.proxied = !record.proxied}/>
-    <Button variant="secondary">Save</Button>
-  </div>
+        {#if type === "A"}
+            <Input bind:value={record.value} label="IPv4 Address"/>
+        {:else if type === "AAAA"}
+            <Input bind:value={record.value} label="IPv6 Address"/>
+        {:else if type === "MX"}
+            <Input type="number" label="Priority" min="0"/>
+            <Input bind:value={record.value} label="Server"/>
+        {:else if type === "NS"}
+            <Input bind:value={record.value} label="Nameserver"/>
+        {:else if type === "TXT"}
+            <Input bind:value={record.value} label="Value"/>
+        {:else if type === "SRV"}
+            <Input label="Service"/>
+            <Select label="Protocol" items={[
+                { value: "TCP", label: "TCP" },
+                { value: "UDP", label: "UDP" },
+                { value: "TLS", label: "TLS" }
+                ]}/>
+            <Input type="number" label="Priority" min="0"/>
+            <Input type="number" label="Weight" min="0"/>
+            <Input type="number" label="Port" min="0"/>
+            <Input bind:value={record.value} label="Target"/>
+        {/if}
+        <Button icon={record.proxied ? "cloud_queue" : "cloud_off"} on:click={() => record.proxied = !record.proxied} variant="secondary"/>
+        <Button variant="secondary">Save</Button>
+    </div>
 </div>
