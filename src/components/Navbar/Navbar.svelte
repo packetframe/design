5f3f331f4460e206616fb16e51f3a2a6cd30226b
Nate Sales <nate@natesales.net>
2021-06-19 16:44:47 -0700

    fix navbar active

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -1,17 +1,19 @@
 <script>
+  import {location} from "svelte-spa-router";
+
   const elements = [
-    { label: "DNS", href: "#", active: true },
-    { label: "Containers", href: "#", active: false },
-    { label: "Proxy", href: "#", active: false },
-    { label: "Account", href: "#", active: false }
+    { label: "DNS", href: "/dashboard/dns" },
+    { label: "Containers", href: "/dashboard/containers" },
+    { label: "Proxy", href: "/dashboard/proxy" },
+    { label: "Account", href: "/dashboard/account" }
   ];
 </script>
 
 <nav class="pf-nav">
   <ul>
     {#each elements as item}
-      <li class={item.active ? "active" : ""}>
-        <a href={item.href}>{item.label}</a>
+      <li class={$location === item.href ? "active" : ""}>
+        <a href={"#" + item.href}>{item.label}</a>
       </li>
     {/each}
   </ul>
