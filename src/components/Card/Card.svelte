9d3414fddc896353ecfe86314bd5843d1e0a6796
Nate Sales <nate@natesales.net>
2021-06-19 18:29:59 -0700

    add card component

---
+++ src/components/Card/Card.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let title = "";
+</script>
+
+<main>
+    <fieldset>
+        <legend>{title}</legend>
+        <slot></slot>
+    </fieldset>
+</main>
+
+<style global lang="scss" src="./Card.scss">
+</style>
