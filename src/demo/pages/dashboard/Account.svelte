4668e3ca9ae6282f144446a12bbe585f5ccf2f8f
Nate Sales <nate@natesales.net>
2021-06-19 18:40:13 -0700

    add button

--- src/demo/pages/dashboard/Account.svelte
+++ src/demo/pages/dashboard/Account.svelte
@@ -1,11 +1,16 @@
 <script>
     import Input from "../../../components/Input";
+    import Card from "../../../components/Card";
+    import Button from "../../../components/Button";
 </script>
 
 <main>
     <h1>Account</h1>
-    <Input label="Username" placeholder="Enter username..."/>
-    <Input label="Password" placeholder="Enter password..." type="password"/>
+    <Card title="Change Password">
+        <Input style="margin-bottom: 10px" fixErrorHeight={false} label="Password" placeholder="Enter password..." type="password"/>
+        <Input fixErrorHeight={false} label="Repeat Password" placeholder="Enter password..." type="password"/>
+        <Button style="margin-top: 18px" variant="secondary">Save</Button>
+    </Card>
 </main>
 
 <style>
@@ -13,5 +18,9 @@
         max-width: 900px;
         margin: 0 auto;
         color: white;
+    }
+
+    h1 {
+        margin-bottom: 10px;
     }
 </style>
