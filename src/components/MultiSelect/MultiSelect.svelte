64a0a972a6cca3b6b987cb838d923543338f241b
Nate Sales <nate@natesales.net>
2021-06-19 22:44:14 -0700

    add containers page

---
+++ src/components/MultiSelect/MultiSelect.svelte
@@ -0,0 +1,31 @@
+<script>
+    import Input from "../Input";
+    import Card from "../Card/Card.svelte";
+    import Button from "../Button/Button.svelte";
+
+    export let values = [];
+
+    let value;
+</script>
+
+<main>
+    <Card title="Ports">
+        <div style="display: flex; align-items: center">
+            <Input label="Ports" placeholder="443:443" bind:value/>
+            <Button icon="add" on:click={() => {
+                values.push(value)
+                alert(values)
+            }}/>
+        </div>
+
+        <span>Ports: (click to delete) {values}</span>
+
+        {#if values.length > 0}
+            <ul>
+                {#each values as item}
+                    <li>{item}</li>
+                {/each}
+            </ul>
+        {/if}
+    </Card>
+</main>
--- src/demo/pages/dashboard/Containers.svelte
+++ src/demo/pages/dashboard/Containers.svelte
@@ -1,8 +1,76 @@
 <script>
+    import Input from "../../../components/Input";
+    import Button from "../../../components/Button";
+
+    let containers = [
+        {hostname: "c1.example.com", image: "packetframe/edge-demo", ports: ["5000:5000"]},
+        {hostname: "c1.example.com", image: "packetframe/edge-demo", ports: ["5000:5000", "80:80"]},
+        {hostname: "c1.example.com", image: "packetframe/edge-demo", ports: ["5000:5000"]},
+        {hostname: "c1.example.com", image: "packetframe/edge-demo", ports: ["5000:5000"]},
+        {hostname: "c1.example.com", image: "packetframe/edge-demo", ports: ["5000:5000"]},
+        {hostname: "c1.example.com", image: "packetframe/edge-demo", ports: ["5000:5000"]},
+        {hostname: "c1.example.com", image: "packetframe/edge-demo", ports: ["5000:5000"]},
+        {hostname: "c1.example.com", image: "packetframe/edge-demo", ports: ["5000:5000"]},
+    ]
 </script>
 
 <main>
-    <div style="max-width: 900px; margin: 0 auto; color: white;">
-        <h1>Containers</h1>
+    <h1>Containers</h1>
+    <div class="pf-containers-inputs">
+        <div style="width: 50%; display: flex; flex-direction: column; align-items: stretch;">
+            <Input label="Hostname"/>
+            <Input label="Image" placeholder="packetframe/edge-demo"/>
+        </div>
+
+        <div class="pf-containers-env">
+            <Input inputClasses="pf-containers-textarea" label="Environment" placeholder="FOO=bar" textarea/>
+        </div>
     </div>
+
+    <Button variant="secondary">Add</Button>
+
+    <main>
+        <table class="pf-record-table">
+            <tr class="pf-record-table__tr">
+                {#each ["Hostname", "Image", "Ports", ""] as head}
+                    <th class="pf-record-table__header">{head}</th>
+                {/each}
+            </tr>
+            <tr class="pf-record-table__spacer"></tr>
+            {#each containers as container, i}
+                <tr class="pf-record-table__spacer"></tr>
+                <tr class="pf-record" class:zebra={i%2 === 0}>
+                    <td class="pf-record__label">{container.hostname}</td>
+                    <td class="pf-record__type">{container.image}</td>
+                    <td class="pf-record__type">{container.ports}</td>
+                    <td class="pf-record__arrow">
+                        <span style="color: #cc0000" class="material-icons-round">delete</span>
+                    </td>
+                </tr>
+            {/each}
+        </table>
+    </main>
 </main>
+
+<style lang="scss">
+  main {
+    max-width: 900px;
+    margin: 0 auto;
+    color: white;
+  }
+
+  :global(.pf-containers-textarea) {
+    height: 100% !important;
+  }
+
+  .pf-containers-env {
+    width: 50%;
+    height: 100%;
+    display: inline-flex;
+    align-items: stretch;
+  }
+
+  .pf-containers-inputs {
+    display: flex;
+  }
+</style>
