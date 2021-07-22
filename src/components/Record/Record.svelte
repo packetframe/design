22f62a8814d1fb8ab6f05b17c37fb4488e0ab242
Seth Knights <knightss@catlin.edu>
2021-07-22 14:26:11 -0700

    feat(RecordTable): selection and deletion configuration and handling integrated fully

--- src/components/Record/Record.svelte
+++ src/components/Record/Record.svelte
@@ -7,25 +7,36 @@
     type: string;
     ttl: number;
     value: string;
+    proxied: boolean;
   };
   export let record: Record;
   export let zebra: boolean = false;
+
+  export let allowSelection: boolean = true;
+  export let allowDeletion: boolean = true;
+
+  export let selectedState: boolean = false;
+  export let handleSelection: (selectedState: boolean, data: Record, e?: MouseEvent) => void = () => {};
+
+
 
   let open: boolean = false;
 </script>
 
 <tr class="pf-record" class:zebra class:open on:click={() => open = !open}>
+  {#if allowSelection}
   <td class="pf-record__checkbox">
     <!-- svelte-ignore a11y-label-has-associated-control -->
     <label class="checkbox">
       <span class="checkbox__input">
-        <input on:click|stopPropagation type="checkbox" name="checkbox" />
+        <input on:click|stopPropagation={(e) => {handleSelection(!selectedState, record, e)}} type="checkbox" name="checkbox" bind:checked={selectedState} />
         <span class="checkbox__control">
           <span class="bg"></span>
         </span>
       </span>
     </label>
   </td>
+  {/if}
   <td class="pf-record__label">{record.label}</td>
   <td id="col2" class="pf-record__type">{record.type}</td>
   <td class="pf-record__ttl">{record.ttl}</td>
--- src/components/RecordTable/RecordTable.svelte
+++ src/components/RecordTable/RecordTable.svelte
@@ -9,20 +9,37 @@
     proxied: boolean,
   }
 
+  export let allowDeletion: boolean = true;
+  export let allowSelection: boolean = true;
+  export let handleSelection: (selectionState: boolean, data: Record, e: MouseEvent) => any = () => {};
+  export let handleDeletion: (data: Record[]) => any = (d) => {
+    console.log(d);
+  };
+
   export let records: Record[] = [];
+  $: selectionStates = records.map(r => {return {data: r, selected: false}});
+
+  let deleteIcon: string = "delete_outline";
 </script>
 
 <main>
   <table class="pf-record-table">
     <tr class="pf-record-table__tr">
-      {#each ["", "Label", "Type", "TTL", "Value", "", ""] as head}
+      {#if allowSelection && allowDeletion}
+      <th on:click={() => {handleDeletion(selectionStates.filter(r => r.selected).map(r => r.data))}} class="pf-record-table__header material-icons" style="display: flex; font-size: 20px; cursor: pointer;">
+        {deleteIcon}
+      </th>
+      {:else if allowSelection}
+      <th class="pf-record-table__header"></th>
+      {/if}
+      {#each ["Label", "Type", "TTL", "Value", "", ""] as head}
         <th class="pf-record-table__header">{head}</th>
       {/each}
     </tr>
     <tr class="pf-record-table__spacer"></tr>
     {#each records as record, i}
       <tr class="pf-record-table__spacer"></tr>
-      <Record {record} zebra={i%2 === 0} />
+      <Record {allowDeletion} {allowSelection} handleSelection={(s, d, e) => {selectionStates[i].selected = s; handleSelection(s, d, e)}} {record} zebra={i%2 === 0} />
     {/each}
   </table>
 </main>
